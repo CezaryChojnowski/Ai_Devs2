5c377b094d75b9c674d52a5b2f3b1e154acf482a
CezaryChojnowski <chojnowski.czarek@gmail.com>
2023-11-24 20:39:03 +0100

    Fix save as context knowledge

--- src/serverOwnApiPro.ts
+++ src/serverOwnApiPro.ts
@@ -43,12 +43,19 @@ class ConversationHandler {
     console.log(`Received question in conversation ${conversationId}: ${question}`);
 
     const conversationMemory = this.conversationState[conversationId] || {};
+    const conversationMemoryValues = Object.values(conversationMemory);
 
+    const stringValuesArray = conversationMemoryValues.map(memory => {
+        return JSON.stringify(memory);
+        });
+    const resultString = stringValuesArray.join(', ');
+        console.log(resultString)
+    
     const messages: Message[] = [
       {
         content: `You answer questions,
         context\`\`\
-        Model knowledge: ${conversationMemory}`,
+        Model knowledge: ${resultString}`,
         role: RoleConstants.SYSTEM
       },
       {
@@ -56,7 +63,8 @@ class ConversationHandler {
         role: RoleConstants.USER
       }
     ];
-
+    console.log("Message")
+    console.log(messages)
     const completionsRequest: CompletionsRequest = {
       messages: messages,
       model: ModelConstants.GPT_4,
@@ -67,7 +75,7 @@ class ConversationHandler {
       const completionsResponse = await OpenApi.completions(completionsRequest) as CompletionsResponse;
       if (completionsResponse.choices && completionsResponse.choices.length > 0) {
         const answer = completionsResponse.choices[0].message.content;
-
+        console.log("Answer: " + answer)
         conversationMemory[question] = answer;
         this.conversationState[conversationId] = conversationMemory;
 
